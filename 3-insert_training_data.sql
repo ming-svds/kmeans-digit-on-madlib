1980b83659ed6ee0729f8c32b8dd15d20dc6dc5a
Ming Tsai <ming@svds.com>
2015-06-28 07:43:43 -0700

    Clean up path name

--- 3-insert_training_data.sql
+++ 3-insert_training_data.sql
@@ -1,3 +1,3 @@
 create table training_data (label smallint, val smallint[]);
-copy training_data from '/home/gpadmin/demo2/1-psql_loadable.txt' DELIMITER '|' CSV;
+copy training_data from '/home/gpadmin/demo/1-psql_loadable.txt' DELIMITER '|' CSV;
 
--- 9-kmeans-closest.sql
+++ 9-kmeans-closest.sql
@@ -1,3 +1,3 @@
 
 
-copy (select data.label, data.test_id, (madlib.closest_column(centroids, val)).column_id as cluster_id from test_data as data, (select centroids from madlib.kmeans_random('avg_2', 'val', 10, 'madlib.dist_norm2', 'madlib.avg')) as centroids order by data.label) to '/home/gpadmin/demo2/11-testout.txt';
+copy (select data.label, data.test_id, (madlib.closest_column(centroids, val)).column_id as cluster_id from test_data as data, (select centroids from madlib.kmeans_random('avg_2', 'val', 10, 'madlib.dist_norm2', 'madlib.avg')) as centroids order by data.label) to '/home/gpadmin/demo/11-testout.txt';
